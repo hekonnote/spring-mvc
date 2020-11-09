48023b17d49db7d5eaeb4acfbcf109075f666d62
Sergey Tsoy <hekonnote@gmail.com>
2020-11-09 14:55:39 +0600

    final

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -9,43 +9,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import java.io.CharArrayReader;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/cars")
 public class CarController {
 
     @Autowired
     private CarService carService;
 
-    private List<Car> cars;
-// работает (вытаскивает весь список машин)
-//    @GetMapping("/cars")
-//    public String getHTMLSelectedCars(Model model) {
-//        cars = carService.getCarList();
-//        model.addAttribute("cars", cars);
-//        cars.forEach(System.out::println);
-//        return "/cars";
-//    }
+    @GetMapping()
+    //    public String getHTMLSelectedCars(HttpServletRequest request, Model model) {
+    //        String count = request.getParameter("count");
 
-    @GetMapping("/cars")
-//    public String getHTMLSelectedCars(HttpServletRequest request, Model model) {
     public String getHTMLSelectedCars(@RequestParam(value = "count", required = false) String count, Model model) {
-//        String count = request.getParameter("count");
-        System.out.println(count);
 
+        List<Car> cars;
         if (count == null) {
             cars = carService.getCarList();
             model.addAttribute("cars", cars);
-            cars.forEach(System.out::println);
+//            cars.forEach(System.out::println);
         } else {
             if (Integer.parseInt(count) > 0) {
                 cars = carService.getSelectedCars(Integer.parseInt(count));
                 model.addAttribute("cars", cars);
-                cars.forEach(System.out::println);
+//                cars.forEach(System.out::println);
             } else {
                 System.out.println("The request contains no parameters");
             }
